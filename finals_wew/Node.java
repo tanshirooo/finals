913bd6115a06ff09e56515c06e058fb9f2340fc7
tanshirooo <cvergara_240000001593@uic.edu.ph>
2025-06-12 22:46:49 +0800

    Update Node.java

--- finals_wew/Node.java
+++ finals_wew/Node.java
@@ -1,12 +1,15 @@
 package finals_wew;
 
 public class Node {
-    char data;   // Character representation of the node
-    String name; // Name of the node
+    char data;        // Usually the first character of the name.
+    String name;      // Person's name.
+    String hobbies;   // Person's hobbies.
+    int age;          // Person's age.
 
-    public Node(char data, String name) {
+    public Node(char data, String name, String hobbies, int age) {
         this.data = data;
         this.name = name;
+        this.hobbies = hobbies;
+        this.age = age;
     }
 }
-
